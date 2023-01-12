1f145ae1f146796da0dac83e408def3487cd6b59
ANaRobe <ana.maria.robe@gmail.com>
2023-01-12 20:26:28 +0000

    create extra SerializerMethodFields for Profile & PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,11 +8,19 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner    
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(owner=user, post=obj).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
-        fields = ['id', 'owner', 'is_owner', 'profile_id', 'title', 'content', 'image', 'profile_image', 'created_on', 'last_edit',]
+        fields = ['id', 'owner', 'is_owner', 'profile_id', 'title', 'content', 'image', 'profile_image', 'created_on', 'last_edit', 'like_id',]
